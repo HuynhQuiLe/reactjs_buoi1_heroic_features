6ff6d0967135dd3312a0f80feaeebd7da43300c8
HuynhQuiLe <lehuynhqui1996@gmail.com>
2023-08-26 17:33:28 +0700

    added copy right

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,11 @@ const Footer = () => {
     <footer className="py-5 bg-dark">
       <div className="container">
         <p className="m-0 text-center text-white">
-          Copyright © Your Website 2023
+          Copyright © Your Website 2023 <br />
+          Code with love by &nbsp;
+          <a target="_blank" href="https://lehuynhqui.vercel.app/">
+            lehuynhqui.vercel.app
+          </a>
         </p>
       </div>
     </footer>
